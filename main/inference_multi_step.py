ccb0959f312f41adf90d60c317507d1465259e6a
mth9406 <mth9406@naver.com>
2022-09-02 11:14:26 +0900

    modified how to do the online-learning

--- main/inference_multi_step.py
+++ main/inference_multi_step.py
@@ -1,9 +1,11 @@
-import os 
+import os
+from re import X 
 import sys 
 import argparse
 import json 
 
 import pandas as pd
+from time import time
 
 import torch
 from torch import nn
@@ -57,9 +59,12 @@ parser.add_argument('--model_path', type= str, default= './data/skt/model',
                     help= 'a path to (save) the model')
 parser.add_argument('--model_configs', type= str, default= './data/skt/model/commandline_args.txt'
                     ,help= 'model configurations (a text file to be dumped as a json)')
-# To test
 parser.add_argument('--model_name', type= str, default= 'latest_checkpoint(online).pth.tar'
                     ,help= 'model name to save', required= False)
+
+# to save predictions and graphs
+parser.add_argument('--save_results', action= 'store_true', 
+                help= 'to save graphs and figures in the model_path') 
 
 args = parser.parse_args() 
 print(args) 
@@ -97,7 +102,7 @@ def main(args):
     train_loader, valid_loader, test_loader \
         = DataLoader(train_data, batch_size = args.batch_size, shuffle = False),\
             DataLoader(valid_data, batch_size = args.batch_size, shuffle = False),\
-            DataLoader(test_data, batch_size = 2*args.fine_tunning_every, shuffle = False)
+            DataLoader(test_data, batch_size = args.fine_tunning_every, shuffle = False)
 
     print("Loading data done!")
 
@@ -164,6 +169,7 @@ def main(args):
 
     te_mse = []; te_r2 = []; te_mae= []
     weights = []
+    time_ellapsed = []
 
     criterion_mask = nn.BCELoss()
     # test_loader_iter = iter(test_loader)
@@ -182,25 +188,41 @@ def main(args):
         'label_mask':None
     }
 
+    predictions = []
+    labels = []
+    graphs = []
+
     for batch_idx, x in enumerate(test_loader): 
 
         x['input'], x['mask'], x['label'], x['label_mask'] \
         = x['input'].to(device), x['mask'].to(device), x['label'].to(device), x['label_mask'].to(device)
+        
+        # test
+        model.eval() 
+        with torch.no_grad():
+            out = model(x, args.beta)
+            preds = out['preds'].detach().cpu().numpy().flatten()
+            label = x['label'].detach().cpu().numpy().flatten()
 
-        # split the data 
-        bs = x['input'].shape[0]
-        split = bs//2
-        for k, v in x.items(): 
-            x_train[k] = x[k][:split,...]
-            x_test[k]  = x[k][split:,...]
+            te_mse.append(mean_squared_error(label, preds))
+            te_mae.append(mean_absolute_error(label, preds))
+            te_r2.append(r2_score(label, preds))
+            weights.append(len(out['preds']))
+
+            # record labels and predictions 
+            predictions.append(out['preds'].detach().cpu()) # bs, c, t, n
+            labels.append(x['label'].detach().cpu()) # bs, c, t, n
+            if out['adj_mat'] is not None: 
+                graphs.append(out['adj_mat'].detach().cpu()) # bs, c, n, n or bs, n, n
 
         model.train()
+        ts = time()
         # feed forward
         with torch.set_grad_enabled(True):
-            out = model(x_train, args.beta)
-            mse_loss = criterion(out['outs_label'], x_train['label'])
+            out = model(x, args.beta)
+            mse_loss = criterion(out['outs_label'], x['label'])
             if out['outs_mask'] is not None: 
-                bce_loss = criterion_mask(out['outs_mask'], x_train['label_mask'])
+                bce_loss = criterion_mask(out['outs_mask'], x['label_mask'])
                 loss = mse_loss + bce_loss
             else: 
                 loss = mse_loss
@@ -214,30 +236,23 @@ def main(args):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-        # test
-        model.eval() 
-        with torch.no_grad():
-            out = model(x_test, args.beta)
-            preds = out['preds'].detach().cpu().numpy().flatten()
-            label = x_test['label'].detach().cpu().numpy().flatten()
-
-            te_mse.append(mean_squared_error(label, preds))
-            te_mae.append(mean_absolute_error(label, preds))
-            te_r2.append(r2_score(label, preds))
-            weights.append(len(x_test))
+        tf = time()
+        time_ellapsed.append(tf-ts)
 
     te_mse = np.array(te_mse)
     te_mae = np.array(te_mae)
     te_r2 = np.array(te_r2)
+    time_ellapsed = np.array(time_ellapsed)
         
     te_mse_mean = np.average(te_mse, weights= weights)
     te_r2_mean  = np.average(te_r2, weights= weights)
     te_mae_mean  = np.average(te_mae, weights=weights)
+    time_ellapsed_mean  = np.average(time_ellapsed, weights=weights)
 
     te_mse_std = np.average((te_mse-te_mse_mean)**2, weights= weights)
     te_r2_std = np.average((te_r2-te_r2_mean)**2, weights= weights)
     te_mae_std = np.average((te_mae-te_mae_mean)**2, weights= weights)
+    time_ellapsed_std = np.average((time_ellapsed-time_ellapsed_mean)**2, weights=weights)
 
     perf = {
         'r2': [te_r2_mean],
@@ -245,8 +260,104 @@ def main(args):
         'mse': [te_mse_mean],
         'r2_std': [te_r2_std],
         'mae_std': [te_mae_std],
-        'mse_std': [te_mse_std]
+        'mse_std': [te_mse_std],
+        'mean_fine_tunning_time': [time_ellapsed_mean],
+        'std_fine_tunning_time': [time_ellapsed_std]
     }
+
+    if args.save_results: 
+        
+        print('saving the predictions...')
+ 
+        # make a path to save a figures 
+        fig_path = os.path.join(args.model_path, 'test/figures')
+        if not os.path.exists(fig_path):
+            print("Making a path to save figures...")
+            print(f"{fig_path}")
+            os.makedirs(fig_path, exist_ok= True)
+        else:
+            print("The path to save figures already exists, skip making the path...")
+
+        # make a path to save a graphs 
+        graph_path = os.path.join(args.model_path, 'test/graphs')
+        if not os.path.exists(graph_path):
+            print("Making a path to save graphs...")
+            print(f"{graph_path}")
+            os.makedirs(graph_path, exist_ok= True)
+        else:
+            print("The path to save graphs already exists, skip making the path...")
+
+        predictions = torch.concat(predictions, dim=0) # num_obs, num_cells, preds_steps, num_time_series
+        predictions = torch.permute(predictions[:,:,-1, :], (1, 0, 2)) # num_cells, num_obs, num_time_series 
+        predictions = predictions.numpy()
+        if args.cache is not None: 
+            # preds = inv_min_max_scaler(preds, args.cache, args.columns)
+            predictions = inv_min_max_scaler_ver2(predictions, args.cache, args.columns)
+
+        labels = torch.concat(labels, dim=0) # num_obs, num_cells, preds_steps, num_time_series
+        labels = torch.permute(labels[:,:,-1, :], (1, 0, 2)) # num_cells, num_obs, num_time_series
+        labels = labels.numpy()
+        num_cells = labels.shape[0]
+        if args.cache is not None: 
+            # labels = inv_min_max_scaler(labels, args.cache, args.columns)
+            labels = inv_min_max_scaler_ver2(labels, args.cache, args.columns)
+
+        graphs = torch.concat(graphs, dim=0) # num_obs, num_cells, num_time_series, num_time_series
+        graphs = torch.permute(graphs, (1, 0, 2, 3)) # num_cells, num_obs, num_time_series, num_time_series
+        graphs = graphs.numpy()         
+        
+        print('The shape of predictions: ', predictions.shape)
+        print('The shape of labels: ', labels.shape)
+        print('The shape of graphs: ', graphs.shape)
+
+        # graph-options
+        options = {
+                    'node_color': 'skyblue',
+                    'node_size': 3000,
+                    'width': 0.5 ,
+                    'arrowstyle': '-|>',
+                    'arrowsize': 20,
+                    'alpha' : 1,
+                    'font_size' : 15
+                }
+        
+        # saving figures: predictions vs labels
+        # saving graphs 
+        for i in tqdm(range(num_cells), total= num_cells):
+            enb_id = args.decoder.get(i)
+            write_csv(args, 'test/predictions', f'predictions_{enb_id}.csv', predictions[i, ...], args.columns)
+            write_csv(args, 'test/labels', f'labels_{enb_id}.csv', labels[i, ...], args.columns)   
+            
+            fig, axes = plt.subplots(len(args.columns), 1, figsize= (10,3*len(args.columns)))
+
+            for j in range(len(args.columns)):
+                col_name = args.columns[j]
+                fig.axes[j].set_title(f'time-seris plot: {col_name}')
+                fig.axes[j].plot(predictions[i,:,j], label= 'prediction')
+                fig.axes[j].plot(labels[i,:,j], label= 'label')
+                fig.axes[j].legend()
+            
+            fig.suptitle(f"Prediction and True label plot of {enb_id}", fontsize=20, position= (0.5, 1.0+0.05))
+            fig.tight_layout()
+            fig_file = os.path.join(fig_path, f'figure_{enb_id}.png')
+            fig.savefig(fig_file)
+        
+            graph_path = os.path.join(args.model_path, f'test/graphs/{enb_id}')
+            os.makedirs(graph_path, exist_ok= True)
+            plt.figure(figsize =(15,15))
+            for j in range(args.graph_time_range):
+                # num_obs, num_time_series, num_time_series
+                graph_file = os.path.join(graph_path, f'{enb_id}_graph_{j}.png') 
+                adj_mat = np.transpose(graphs[i, j, ...]) # num_time_series, num_time_series 
+                adj_mat = pd.DataFrame(adj_mat, columns = args.columns, index= args.columns)
+                # save the adj-matrix in csv format 
+                adj_mat.to_csv(os.path.join(graph_path, f'{enb_id}_graph_{j}.csv'))
+                G = nx.from_pandas_adjacency(adj_mat, create_using=nx.DiGraph)
+                G = nx.DiGraph(G)
+                pos = nx.circular_layout(G)
+                nx.draw_networkx(G, pos=pos, **options)
+                plt.savefig(graph_file, format="PNG")
+            plt.close('all')
 
     return perf
 
